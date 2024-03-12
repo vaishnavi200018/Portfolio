84142f46d6592fc88eda0a404ba96521cff43446
vaishnavi2000 <vaishnavi200018@gmail.com>
2024-03-12 16:40:18 +0530

    message

--- files/script.js
+++ files/script.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed(".text", {
-    strings: ["Programming" , "Cybersecurity" , "Web Development"],
+    strings: ["SoftWare Programming"  , "Web Development"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
